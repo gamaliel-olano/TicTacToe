7ccf06a94bc84fda09a9b9a88a2e34d3e2d97630
Gama <gmlploln@gmail.com>
2023-11-28 15:03:56 +0800

    Add function of playing out the game

--- game.py
+++ game.py
@@ -17,15 +17,26 @@ class TicTacToe:
     
     def available_moves(self):
         return [i for (i, spot) in enumerate(self.board) if spot == ' ']
-        # moves = []
-        # for (i, spot) in enumerate(self.board):
-        #     # ['x', 'x', 'o'] --> [(0, 'x'), (1, 'x'), (2, 'o')]
-        #     if spot == ' ':
-        #         moves.append(i)
-        # return moves
+    
+    def empty_squares(self):
+        return ' ' in self.board
+    
+    def num_empty_squares(self):
+        return self.board.count(' ')
 
-game = TicTacToe()
-game.print_board()
-print()
-game.print_board_nums()
-print(game.available_moves())
+def play(game, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X' # starting letter
+    # iterate while the game still has empty squares
+    # (we don't have to worry about winner because wi'll just return that
+    # which breaks the loop)
+    while game.empty_squares():
+        # get the move from the appropriate player
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+
+        # let's define a function to make a move!
