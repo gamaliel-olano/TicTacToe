cb4834e561ff7290236ed78755f5177a61b86ee0
Gama <gmlploln@gmail.com>
2023-11-30 22:24:38 +0800

    Stop printing tie after every move

--- game.py
+++ game.py
@@ -96,8 +96,8 @@ def play(game, x_player, o_player, print_game=True):
             # after we made our move, we need to alternate letters
             letter = 'O' if letter == 'X' else 'X'  # switches player
         
-        if print_game:
-            print("It's a tie!")
+    if print_game:
+        print("It's a tie!")
 
 if __name__ == '__main__':
     x_player = HumanPlayer('X')
